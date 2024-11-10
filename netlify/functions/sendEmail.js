4e6b83b887cb72398d30b6cf979125b20c6da08b
Muhammed Shahir M S <shahirshamsudheen@gmail.com>
2024-11-10 21:19:36 +0530

    updated

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -5,7 +5,7 @@ import sgMail from '@sendgrid/mail'
 exports.handler = async function (event) {
   const { name, email, message } = JSON.parse(event.body);
 
-  console.log('The API key we talk about is', process.env.REACT_APP_SENDGRID_API_KEY);
+  console.log('The reciever we talk about is',  process.env.REACT_APP_RECEIPENT_EMAIL);
   
 
   sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
@@ -14,7 +14,7 @@ exports.handler = async function (event) {
   const msg = {
     to: process.env.REACT_APP_RECEIPENT_EMAIL,
     replyTo : email,
-    from: process.env.REACT_APP_SENDER_EMAIL,  // verified sender email on SendGrid
+    from: process.env.REACT_APP_SENDER_EMAIL,
     subject: `New message from ${name}`,
     text: message
   };
--- src/services/productService.js
+++ src/services/productService.js
@@ -6,7 +6,7 @@ productService.getProducts = () => {
     console.log('Hii', client);
     
     return client
-    .getEntries({ content_type: 'product' })
+    .getEntries({ content_type: 'product',   'metadata.tags.sys.id[in]': 'pachakam' })
 }
 
 export default productService
