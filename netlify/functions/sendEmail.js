5e846cb559588918c21b256cab8c4f381dff98dd
Muhammed Shahir M S <shahirshamsudheen@gmail.com>
2024-11-12 07:16:58 +0530

    updated

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -5,7 +5,14 @@ import sgMail from '@sendgrid/mail'
 exports.handler = async function (event) {
   const { name, email, message } = JSON.parse(event.body);
 
-  console.log('The reciever we talk about is',  process.env.REACT_APP_RECEIPENT_EMAIL);
+  console.log('The msg we talk about is',  {
+    to: process.env.REACT_APP_RECEIPENT_EMAIL,
+    replyTo : email,
+    from: process.env.REACT_APP_SENDER_EMAIL,
+    subject: `New message from ${name}`,
+    text: message
+  }
+  );
   
 
   sgMail.setApiKey(process.env.REACT_APP_SENDGRID_API_KEY);
@@ -27,7 +34,7 @@ exports.handler = async function (event) {
       body: JSON.stringify({ success: true, message: 'Email sent successfully' }),
     };
   } catch (error) {
-    console.error('Error sending email:', error);
+    console.error('Error sending email catched :', error);
     return {
       statusCode: 500,
       body: JSON.stringify({ success: false, error: 'Failed to send email' }),
