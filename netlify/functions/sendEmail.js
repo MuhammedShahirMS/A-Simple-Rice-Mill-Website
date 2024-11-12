62524ccc262ef9fc67c384ff6f71430619b7a799
Muhammed Shahir M S <shahirshamsudheen@gmail.com>
2024-11-12 07:25:20 +0530

    updated

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -34,10 +34,32 @@ exports.handler = async function (event) {
       body: JSON.stringify({ success: true, message: 'Email sent successfully' }),
     };
   } catch (error) {
-    console.error('Error sending email catched :', error);
+    console.error('Error sending email catched :', error?.response?.body?.errors?.map(item => JSON.stringify(item)));
+    
     return {
       statusCode: 500,
       body: JSON.stringify({ success: false, error: 'Failed to send email' }),
     };
   }
 };
+
+
+const errortraced = {
+  code: 403,
+  response: {
+    headers: {
+      server: 'nginx',
+      date: 'Tue, 12 Nov 2024 01:50:24 GMT',
+      'content-type': 'application/json',
+      'content-length': '281',
+      connection: 'close',
+      'access-control-allow-origin': 'https://sendgrid.api-docs.io',
+      'access-control-allow-methods': 'POST',
+      'access-control-allow-headers': 'Authorization, Content-Type, On-behalf-of, x-sg-elas-acl',
+      'access-control-max-age': '600',
+      'x-no-cors-reason': 'https://sendgrid.com/docs/Classroom/Basics/API/cors.html',
+      'strict-transport-security': 'max-age=600; includeSubDomains'
+    },
+    body: { errors: [Array] }
+  }
+}
